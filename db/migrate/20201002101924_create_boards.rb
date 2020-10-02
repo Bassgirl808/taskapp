70a6a5da64b52359a036e510ca9b325c4a40e68b
Ubuntu <ubuntu@ip-172-31-6-211.us-east-2.compute.internal>
2020-10-02 10:26:25 +0000

    Feat: Create Boards and database schema

--- db/migrate/20201002101924_create_boards.rb
+++ db/migrate/20201002101924_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration[5.0]
   def change
     create_table :boards do |t|
       t.string :name
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
