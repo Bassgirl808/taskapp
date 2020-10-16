ec77a9640016a110c0617ec6fc7977462c2e0246
Ubuntu <ubuntu@ip-172-31-6-211.us-east-2.compute.internal>
2020-10-16 04:35:50 +0000

    Feat: Configure devise default routes and home to boards index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations'}
+
+  devise_scope :user do
+    authenticated :user do
+      root 'boards#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :tasks
   resources :boards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
